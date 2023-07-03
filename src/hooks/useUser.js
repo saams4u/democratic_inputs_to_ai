2ca57e31ff0290844068aa78c630542ec5d82788
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2023-07-02 23:47:31 -0400

    fixing deployment issues with Vercel

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 
 export default function useUser() {
     const [user, setUser] = useState(null);
+    const baseUrl = "https://democratic-inputs-to-ai-3bv6-1kf531lwp-saams4u.vercel.app";
 
     useEffect(() => {
         const getUser = async () => {
-            const res = await fetch("/api/user");
+            const res = await fetch(`${baseUrl}/api/user`);
             const data = await res.json();
             setUser(data);
         }
