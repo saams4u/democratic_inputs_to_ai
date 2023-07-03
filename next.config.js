5cdcd7c1599bcab09747dbc9c253f46254c2a9b2
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2023-07-03 11:16:48 -0400

    fixing deployment issues with Vercel

--- next.config.js
+++ next.config.js
@@ -1,21 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ["upload.wikimedia.org"]
-    },
-    async headers() {
-      return [
-        {
-          source: '/api/:path*',
-          headers: [
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-            { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }
-          ],
-        },
-      ];
-    },
+  reactStrictMode: true,
+  images: {
+      domains: ["upload.wikimedia.org"]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { 
+            key: "Access-Control-Allow-Credentials", value: "true" 
+          },
+          { 
+            key: 'Access-Control-Allow-Origin', 
+            value: 'https://democratic-inputs-to-ai-3bv6.vercel.app' 
+          },
+          { 
+            key: 'Access-Control-Allow-Methods', 
+            value: 'GET,POST,PUT,DELETE,OPTIONS' 
+          },
+          { 
+            key: 'Access-Control-Allow-Headers', 
+            value: "Content-Type, Authorization",
+          }
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig
