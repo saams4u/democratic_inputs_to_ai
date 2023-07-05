56fcc042518a122441d4f255058637f766636464
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2023-07-05 15:29:14 -0400

    fixing deployment issues with Vercel

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,12 +1,31 @@
 
-import { withNextSession } from "@/lib/session";
+import { connectDb } from "@/lib/db";
 
-export default withNextSession(userRoute);
+export default async function userRoute(req, res) {
+  if (req.method === 'GET') {
+    const { username } = req.cookies;
 
-function userRoute(req, res) {
-  if (req.session.user) {
-    return res.status(200).json(req.session.user);
+    if (!username) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const client = await connectDb();
+    const db = client.db();
+
+    try {
+      const user = await db.collection('users').findOne({ username });
+
+      if (user) {
+        const { password, ...userData } = user;
+        return res.status(200).json(userData);
+      } else {
+        return res.status(404).json({ error: 'User not found' });
+      }
+    } finally {
+      client.close();
+    }
   } else {
-    return res.status(404).json({ error: 'User not found' });
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
